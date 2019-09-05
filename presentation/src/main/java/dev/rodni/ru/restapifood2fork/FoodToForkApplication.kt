f0e263e682ab3052cc8ce2491ff70ec7a154dfb7
rodniw <rodion.prosvirnin@bk.ru>
2019-09-05 13:47:47 +0300

    added dependencies.

--- presentation/src/main/java/dev/rodni/ru/restapifood2fork/FoodToForkApplication.kt
+++ presentation/src/main/java/dev/rodni/ru/restapifood2fork/FoodToForkApplication.kt
@@ -1,11 +1,30 @@
 package dev.rodni.ru.restapifood2fork
 
-import android.app.Application
+import android.content.Context
 import android.util.Log
+import androidx.multidex.MultiDex
 import com.jakewharton.threetenabp.AndroidThreeTen
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
 
-class FoodToForkApplication: Application() {
-    private val TAG = "FoodToForkApplication"
+class FoodToForkApplication: DaggerApplication() {
+    companion object {
+        private const val TAG = "FoodToForkApplication"
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        TODO("not implemented")
+    }
+
+    /**
+     * Versions of the platform prior to Android 5.0 (API level 21) use the Dalvik runtime for executing app code.
+     * By default, Dalvik limits apps to a single classes.dex bytecode file per APK.
+     * In order to get around this limitation, we can add the multidex support library to project.
+     */
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
